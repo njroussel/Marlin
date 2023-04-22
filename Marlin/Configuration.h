55122ce0a611629dbda70fe88af79a960a88955b
Nicolas Roussel <n.roussel13@gmail.com>
2023-04-22 18:36:25 +0200

    Update bed PIDs, enable linear advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -752,9 +752,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp  160.43
-  #define DEFAULT_bedKi   14.09
-  #define DEFAULT_bedKd 1217.88
+  #define DEFAULT_bedKp  315.76
+  #define DEFAULT_bedKi   45.35
+  #define DEFAULT_bedKd 1465.86
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
